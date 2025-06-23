288bbd304202bd05c3520c9bbf2281cbd3bb59ea
unknown <lucaszanchin23@gmail.com>
2025-06-23 14:29:30 -0300

    Parte3 Corrigida

--- index.js
+++ index.js
@@ -17,59 +17,6 @@ let distancia
 console.log("Sistema de Cálculo de Entregas ");
 
 while (continuar) { //loop que permite ao usuário cadastrar várias entregas.
-    console.log("\nNova entrega:");
-    do {
-        nome = prompt("Nome do cliente: "); // Pede ao cliente para digitar o seu nome.
-        if (!isNaN(nome) || nome == "") {
-            console.log("Inválido! Você não digitou um nome, escreva novamente!"); // Indica se a entrada de informações não foi compatível com o pedido.
-        }
-    } while (!isNaN(nome) || nome == ""); // Repetição do looping.
-
-    do {
-        endereco = prompt("Endereço de Entrega:");  //Essa variável pede para o usuário o endereço dele
-        if (!isNaN(endereco) || endereco == "") {
-            console.log("Inválido! Você não digitou um endereço, escreva novamente!"); // Indica se a entrada de informações não foi compatível com o pedido.
-        }
-    } while (!isNaN(endereco) || endereco == ""); // Repetição do looping.
-
-    do {
-        distancia = parseFloat(prompt('Distância da entrega (km): ')); //Essa variável pede para o usuário a distância da entrega
-        if (isNaN(distancia) || distancia == "") {
-            console.log("Inválido! Você não digitou um número, escreva novamente!"); // Indica se a entrada de informações não foi compatível com o pedido.
-        }
-    } while (isNaN(distancia) || distancia == ""); // Repetição do looping.
-
-    do {
-        valorPorKm = parseFloat(prompt('Valor cobrado por km: ')); //Essa variável pede o valor por quilômetro
-        if (isNaN(valorPorKm) || valorPorKm == "") {
-            console.log("Inválido! Você não digitou um número, escreva novamente!"); // Indica se a entrada de informações não foi compatível com o pedido.
-        }
-    } while (isNaN(valorPorKm) || valorPorKm == ""); // Repetição do looping.
-
-    let tipo = ""; //Essa variável pede para o usuário o tipo de entrega
-    let tipoValido = false; //Essa variável verifica se o tipo é válido ou não
-
-    while (!tipoValido) {  // loop de validação que garante que o usuário insira um tipo de entrega válido
-        tipo = prompt('Tipo de entrega (normal ou urgente): '); //Pergunta ao usuário qual tipo de entrega
-        if (tipo === 'normal' || tipo === 'urgente') { //Verifica se o valor inserido pelo usuário é igual a "normal" ou "urgente"
-            tipoValido = true; //Isso define que "tipoVálido" seja verdadeiro assim encerrando o loop, pois enquanto for falso vai se repetir até ser verdadero e se encerrar
-        } else {
-            console.log('Tipo inválido! Digite "normal" ou "urgente".'); //Messagem que será enviada ao usuário se a resposta for inválida
-        }
-    }
-
-    let custo = distancia * valorPorKm; //Variável que fará a multiplicação da distância pelo valor por KM
-    if (tipo === 'urgente') { //É uma condição que verifica se a variável tipo contém o valor 'urgente'
-        custo *= 1.2;  //Aqui será feita multiplicação de um custo a mais caso seja urgente
-    }
-
-    nomes[i] = nome; //O que será armazenado na variável "nome" irá pro vetor "nomes"
-    enderecos[i] = endereco; //O que será armazenado na variável "endereço" irá pro vetor "endereços"
-    distancias[i] = distancia; //O que será armazenado na variável "distância" irá pro vetor "distâncias"
-    valoresPorKm[i] = valorPorKm; //O que será armazenado na variável "valorPorKM" irá pro vetor "valoresPorKM"
-    tiposEntrega[i] = tipo; //O que será armazenado na variável "tipo" irá pro vetor "tiposEntrega"
-    custos[i] = custo; //O que será armazenado na variável "custo" irá pro vetor "custos"
-
     console.log("\nResumo da entrega:"); //Mostra o resumo da entrega
     console.log(`Cliente: ${nome}`); //Mostra o nome do cliente
     console.log(`Endereço: ${endereco}`); //Mostra o endereço do cliente
